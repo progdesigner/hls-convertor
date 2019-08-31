87bc6457573816db0df78122514f3a6fe4d4d9b8
progdesigner <me@progdesigner.com>
2019-08-31 09:34:47 +0900

    First Commit
    - 쉘 설치

---
+++ bin/generator.sh
@@ -0,0 +1,284 @@
+#/bin/bash
+
+VERSION="0.1.1"
+
+EXEC_FILE="$0"
+BASE_NAME=`basename "$EXEC_FILE"`
+PATH_CURRENT=`pwd`
+PATH_BASE=${PATH_CURRENT}
+{
+	if [ "$EXEC_FILE" = "./$BASE_NAME" ] || [ "$EXEC_FILE" = "$BASE_NAME" ]; then
+		PATH_BASE=`pwd`
+	else
+		PATH_BASE=`echo "$EXEC_FILE" | sed 's/'"${BASE_NAME}"'$//'`
+	    cd "${PATH_BASE}" > /dev/null 2>&1
+	    PATH_BASE=`pwd`
+	fi
+
+	cd "${PATH_BASE}/.."
+	export PATH_BASE=`pwd`
+	cd ${PATH_CURRENT}
+}
+
+PATH_BIN="${PATH_BASE}/bin"
+PATH_SRC="${PATH_CURRENT}"
+PATH_DIST="${PATH_CURRENT}/dist"
+
+function before_convert() {
+  mkdir -p ${PATH_DIST}
+  mkdir -p ${PATH_SRC}
+}
+
+function convert_gif() {
+
+  echo "Convert PNGs to GIF" $@
+
+  # Default Options
+  FILE_SOURCE="*.png"
+  FILE_DIST_NAME="animated"
+  BACKGROUND=""
+  DELAY=0
+  ARGS=()
+
+  while [[ $# -gt 0 ]]
+  do
+    PARAM="$1"
+    case $PARAM in
+        -s|--source)
+          FILE_SOURCE="$2"
+          shift # past argument
+          shift # past value
+        ;;
+
+        -o|--dist)
+          FILE_DIST_NAME="$2"
+          shift # past argument
+          shift # past value
+        ;;
+
+        -b|--background)
+          BACKGROUND="$2"
+          shift # past argument
+          shift # past value
+        ;;
+
+        -d|--delay)
+          DELAY=$2
+          shift # past argument
+          shift # past value
+        ;;
+
+        *)    # unknown option
+          ARGS+=($1)
+          shift # past argument
+        ;;
+    esac
+  done
+
+  # Variables
+  FILE_DIST="${FILE_DIST_NAME}.gif"
+  SEQUENCES=(`find ${PATH_SRC} -type f -name "${FILE_SOURCE}" | sort -t'/' -k2.1 -k2.2r`)
+  COUNT="${#SEQUENCES[@]}"
+  CONVERT_OPTIONS="-delay 0 -loop 0 -layers optimize-frame -layers optimize-transparency"
+
+  if [ "${BACKGROUND}" != "" ]; then
+    CONVERT_OPTIONS+=" -background ${BACKGROUND} -alpha remove -alpha off"
+  fi
+
+  if [ "${COUNT}" = "0" ]; then
+    echo "Resource Files isn't exists."
+    exit 0
+  fi
+
+  # Create Backup
+  if [ -e "${PATH_DIST}/${FILE_DIST}" ]; then
+    rm -rf "${PATH_DIST}/${FILE_DIST}.backup"
+    mv ${PATH_DIST}/${FILE_DIST} "${PATH_DIST}/${FILE_DIST}.backup"
+  fi
+
+  # Generate GIF
+  echo "Generating GIF..."
+  # info: http://www.imagemagick.org/script/command-line-options.php?#layers
+  convert ${CONVERT_OPTIONS} ${SEQUENCES[*]} "${PATH_DIST}/${FILE_DIST}"
+  echo "Generated GIF"
+
+  FILE_SIZE=`du -k "${PATH_DIST}/${FILE_DIST}" | cut -f1`
+  echo "FileSize: ${FILE_SIZE}kb"
+}
+
+function convert_webp() {
+
+  echo "Convert PNGs to WebP"
+
+  # Default Options
+  FILE_SOURCE="*.png"
+  FILE_DIST_NAME="animated"
+  DELAY=1
+  QUALITY=50
+  ARGS=()
+
+  while [[ $# -gt 0 ]]
+  do
+    PARAM="$1"
+    case $PARAM in
+        -s|--source)
+          FILE_SOURCE="$2"
+          shift # past argument
+          shift # past value
+        ;;
+
+        -o|--dist)
+          FILE_DIST_NAME="$2"
+          shift # past argument
+          shift # past value
+        ;;
+
+        -q|--quality)
+          QUALITY=$2
+          shift # past argument
+          shift # past value
+        ;;
+
+        -d|--delay)
+          if [ $2 -ge 1 ]; then
+            DELAY=$2
+          fi
+          shift # past argument
+          shift # past value
+        ;;
+
+        *)    # unknown option
+          ARGS+=($1)
+          shift # past argument
+        ;;
+    esac
+  done
+
+  # Variables
+  FILE_DIST="${FILE_DIST_NAME}.webp"
+  SEQUENCES=(`find ${PATH_SRC} -type f -name "${FILE_SOURCE}" | sort -t'/' -k2.1 -k2.2r`)
+  COUNT="${#SEQUENCES[@]}"
+  FILE_OPTIONS="-loop 0"
+  PRE_OPTIONS="-q ${QUALITY} -d ${DELAY} ${ARGS[*]}"
+
+  if [ "${COUNT}" = "0" ]; then
+    echo "Resource Files isn't exists."
+    exit 0
+  fi
+
+  # Create Backup
+  if [ -e "${PATH_DIST}/${FILE_DIST}" ]; then
+    rm -rf "${PATH_DIST}/${FILE_DIST}.backup"
+    mv ${PATH_DIST}/${FILE_DIST} "${PATH_DIST}/${FILE_DIST}.backup"
+  fi
+
+  # Generate WebP
+  echo "Generating WebP..."
+  img2webp ${FILE_OPTIONS} ${SEQUENCES[*]} -o "${PATH_DIST}/${FILE_DIST}" ${PRE_OPTIONS}
+  echo "Generated WebP"
+
+  FILE_SIZE=`du -k "${PATH_DIST}/${FILE_DIST}" | cut -f1`
+  echo "FileSize: ${FILE_SIZE}kb"
+}
+
+function cli_update() {
+  mkdir -p "/usr/local/Cellar/animation-generator"
+  rsync -av "${PATH_BASE}/" "/usr/local/Cellar/animation-generator/" --exclude=dist --exclude=src --exclude=.git --exclude=.gitignore
+  chmod +x "/usr/local/Cellar/animation-generator/bin/generator.sh"
+}
+
+function help() {
+  echo "Animation Generator ${VERSION}"
+  echo "Copyright: © 2019 ProgDesigner."
+  echo "MIT License"
+  echo "Usage: gripg [options ...] file [ [options ...] file ...] [options ...] file"
+  echo ""
+  echo "Options Settings:"
+  echo "  -s|--source             source path"
+  echo "  -o|--dist               distribution path"
+  echo "  -d|--delay value        display the next image after pausing"
+  echo "  -q|--quality value      specify the compression factor between 0 and 100. The default is 75."
+  echo "  -b|--background color   background color"
+  echo "                            for example,"
+  echo "                              blue"
+  echo "                              \"#ddddff\""
+  echo "                              \"rgb(255, 255, 255)\""
+  echo ""
+}
+
+function execute() {
+  COMMAND=$1
+  shift
+
+  ARGS=$@
+
+  case ${COMMAND} in
+    install)
+
+      if command -v grip > /dev/null; then
+        echo "grip is installed"
+        exit 0
+      fi
+
+      # if command -v brew > /dev/null; then
+      #   echo "brew is installed"
+      # else
+      #   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" < /dev/null 2> /dev/null
+      # fi
+      #
+      # if brew ls --versions webp > /dev/null; then
+      #   echo "WebP is installed"
+      # else
+      #   brew install webp
+      # fi
+      #
+      # if brew ls --versions imagemagick > /dev/null; then
+      #   echo "ImageMagick is installed"
+      # else
+      #   brew install webp imagemagick
+      # fi
+
+      cli_update
+      cd "/usr/local/bin"
+      ln -s "../Cellar/animation-generator/bin/generator.sh" grip
+    ;;
+
+    update)
+      cli_update
+    ;;
+
+    gif)
+      before_convert
+
+      convert_gif $ARGS
+
+      # Open Folder
+      open -a "Finder" "${PATH_DIST}"
+    ;;
+
+    webp)
+      before_convert
+
+      convert_webp $ARGS
+
+      # Open Folder
+      open -a "Finder" "${PATH_DIST}"
+    ;;
+
+    all)
+      before_convert
+
+      convert_webp $ARGS
+      convert_gif $ARGS
+
+      # Open Folder
+      open -a "Finder" "${PATH_DIST}"
+    ;;
+
+    *)
+      help
+    ;;
+  esac
+}
+
+execute $@
---
+++ bin/install.sh
@@ -0,0 +1,71 @@
+#/bin/bash
+
+# EXECUTE: /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/progdesigner/animation-generator/bin/install.sh)"
+
+PATH_LIB="/usr/local/Cellar"
+LIB_NAME="animation-generator"
+
+function execute() {
+  COMMAND="$0"
+  SKIP=0
+  OPTIONS=()
+
+  if [ "$COMMAND" == "--skip" ]; then
+    SKIP=1
+  fi
+
+  while [[ $# -gt 0 ]]
+  do
+    PARAM="$1"
+    case $PARAM in
+      --skip)
+        SKIP=1
+        shift # past value
+      ;;
+
+      *) # unknown option
+        OPTIONS+=($1)
+        shift # past argument
+      ;;
+    esac
+  done
+
+
+  if [ $SKIP -lt 1 ]; then
+    if command -v grip > /dev/null; then
+      echo "grip is installed"
+      exit 0
+    fi
+  fi
+
+  if command -v brew > /dev/null; then
+    echo "brew is installed"
+  else
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" < /dev/null 2> /dev/null
+  fi
+
+  if brew ls --versions webp > /dev/null; then
+    echo "WebP is installed"
+  else
+    brew install webp
+  fi
+
+  if brew ls --versions imagemagick > /dev/null; then
+    echo "ImageMagick is installed"
+  else
+    brew install webp imagemagick
+  fi
+
+  rm -rf ${PATH_LIB}/${LIB_NAME}
+  mkdir -p ${PATH_LIB}/${LIB_NAME}
+  cd ${PATH_LIB}
+  git clone git@github.com:progdesigner/animation-generator.git ${LIB_NAME}
+
+  # rsync -av "${PATH_BASE}/" "/usr/local/Cellar/animation-generator/" --exclude=dist --exclude=src --exclude=.git --exclude=.gitignore
+
+  chmod +x "${PATH_LIB}/${LIB_NAME}/bin/generator.sh"
+  cd "/usr/local/bin"
+  ln -s "../Cellar/animation-generator/bin/generator.sh" grip
+}
+
+execute $@
